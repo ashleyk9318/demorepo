c33c8eae1a2f56e59c2d51ba155d295f01971b87
Jonathan Disher <jdisher@marklarheavyindustries.net>
2014-11-14 15:55:05 -0800

    Initial commit

---
+++ data structures group project/data structures group project/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  data structures group project
+//
+//  Created by Jonathan Disher on 11/14/14.
+//  Copyright (c) 2014 Jonathan Disher. All rights reserved.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, De Anza!\n";
+    return 0;
+}
